f3896c3494023b96d7bc006fe3080e520b8eb763
Joseemmanuel <emmanuelriv15@gmail.com>
2023-09-18 10:36:36 -0600

    Finish Certificates #6

--- src/app/services/certificados.service.ts
+++ src/app/services/certificados.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CertificadosService {
+  constructor(private readonly http: HttpClient) {}
+  apiURL = 'http://localhost:8080/certificates';
+  //http opcions
 
-  constructor() { }
+  httpOpcions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  getCertificates(): Observable<any> {
+    return this.http
+      .get<any>(this.apiURL, this.httpOpcions)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error code ${error.status} Mensage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 }
