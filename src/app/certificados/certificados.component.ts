ce947e37d0f2649169070783dad6a5eb94727ea7
Emmanuel Rivera <129634455+emmannem@users.noreply.github.com>
2023-09-18 11:09:43 -0600

    Certificados Finish #6 (#15)
    * Modified Certificates #6
    
    * Finish Certificates #6
    
    * Update Certificates #6

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CertificadosService } from '../services/certificados.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { CertificadosService } from '../services/certificados.service';
   templateUrl: './certificados.component.html',
   styleUrls: ['./certificados.component.css'],
 })
-export class CertificadosComponent {
+export class CertificadosComponent implements OnInit {
   certificados: Array<any> = [];
   constructor(private certificadoService: CertificadosService) {}
 
