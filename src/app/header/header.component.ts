dcea5014f63429fa5d36c6cd138e43e8293d7675
Joseemmanuel <emmanuelriv15@gmail.com>
2023-09-18 00:29:39 -0600

    Modified Header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  name: string = 'Fake Nombre';
+  titulos: string = 'Fake Titulos';
+  mision: string = 'Fake Misión';
+  fotoUrl: string = '';
+  email: string = 'Fake Email';
+  celular: string = 'Fake Celular';
+  ubicacion: string = 'Fake Ubicación';
+  redsocial: string = 'Fake Red Social';
 
+  constructor(private headerservice: HeaderService) {}
+
+  ngOnInit(): void {
+    this.headerservice.getHeader().subscribe((data: any) => {
+      this.name = data.name;
+      this.titulos = data.titulos;
+      this.mision = data.mision;
+      this.fotoUrl = data.foto;
+      this.email = data.email;
+      this.celular = data.celular;
+      this.ubicacion = data.ubicacion;
+      this.redsocial = data.redsocial;
+      console.log(data);
+      //alert(data);
+    });
+  }
 }
